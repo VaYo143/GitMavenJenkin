3d68cebb860bc1ddcc2d55ea1e32137f056d37e2
VaYo143 <VaYo143@gmail.com>
2020-06-16 17:18:56 +0530

    ok

--- Jenkin/src/main/java/com/capgemini/Jenkin/App.java
+++ Jenkin/src/main/java/com/capgemini/Jenkin/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldd!" );
+        System.out.println( "Hello Worldd!!" );
         System.out.println( "Hello India!" );
     }
 }
